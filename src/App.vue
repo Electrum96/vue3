375f1085e421de9c371c5aa830f892e90527257b
Electrum <kachalina.kate96@mail.ru>
2024-02-29 14:51:11 +0300

    'made PostList and PostForm components, used props'

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,55 @@
 <template>
-  <div>
-      <form>
-        <h4>Создание поста</h4>
-        <input class="input" type="text" placeholder="Название">
-        <input class="input" type="text" placeholder="Описание">
-        <button>Создать</button>
-      </form>
-    <div class="post" v-for="post in posts">
-      <div><strong>Название:</strong>{{post.title}}</div>
-      <div><strong>Описание:</strong>{{post.body}}</div>
-
-    </div>
+  <div class="app">
+    <post-form/>
+    <post-list :posts="posts"/>  <!--/ : сокращенная запись директивы v-bind передает пропсы в компонет-->
   </div>
 </template>
-
-<script> //логика компонента , по деволту экспортируется объект
-export default {
-  data() {
+<script> /*логика компонента */
+import PostForm from "@/components/PostForm.vue"; /*импорты компонентов*/
+import PostList from "@/components/PostList.vue";
+export default { /* по дефолту экспортируется объект*/
+  components: {
+    PostList, PostForm ///регистрация импортированных объектов
+  },
+  data() { /*массив постов*/
       return {
         posts: [
           {id:1, title: 'Пост', body: 'Описание поста'},
           {id:2, title: 'Пост 2', body: 'Описание поста 2'},
           {id:3, title: 'Пост 3', body: 'Описание поста 3'},
-        ]
+        ],
+        title: '',
+        body: ''
       }
   },
-  methods: {
+  methods: { /*в методы пишутся функции*/
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body
+      }
+          this.posts.push(newPost);
+          this.title = '';
+          this.body = '';
+
+    },
 
   }
 }
 </script>
 
 <style>
-* {
+* {  /*обнуление стилей*/
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
 
-.post {
-  padding: 15px;
-  border: 2px solid teal;
-  margin-top: 15px;
+.app {
+  padding:  20px;
 }
-.input {
-  width: 100%;
-  border: 1px;
-}
+
+
+
 </style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,30 @@
+<template>
+<!--  //компонент Лист получает на вход массив и рисует items//-->
+  <div class="post" v-for="post in posts">
+    <div><strong>Название:</strong>{{post.title}}</div>
+    <div><strong>Описание:</strong>{{post.body}}</div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  /*//передаются пропсы //*/
+  props:{
+    posts: {
+      type: Array,
+      required: true
+    }
+  }
+
+}
+</script>
+
+<style scoped> /*//scoped что данные стили применяются толь ко внутри данного компонента//*/
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+
+</style>
