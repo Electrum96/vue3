b830d4f0b75035fa24ba8460816b19d47a718ad4
Electrum <kachalina.kate96@mail.ru>
2024-02-29 12:58:06 +0300

    'made root component'

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+      <form>
+        <h4>Создание поста</h4>
+        <input class="input" type="text" placeholder="Название">
+        <input class="input" type="text" placeholder="Описание">
+        <button>Создать</button>
+      </form>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название:</strong>{{post.title}}</div>
+      <div><strong>Описание:</strong>{{post.body}}</div>
+
+    </div>
+  </div>
 </template>
 
+<script> //логика компонента , по деволту экспортируется объект
+export default {
+  data() {
+      return {
+        posts: [
+          {id:1, title: 'Пост', body: 'Описание поста'},
+          {id:2, title: 'Пост 2', body: 'Описание поста 2'},
+          {id:3, title: 'Пост 3', body: 'Описание поста 3'},
+        ]
+      }
+  },
+  methods: {
+
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.input {
+  width: 100%;
+  border: 1px;
 }
 </style>
