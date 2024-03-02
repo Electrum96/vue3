932aede98d28020fb2631a5fe792e9558cc16c08
Electrum <kachalina.kate96@mail.ru>
2024-03-02 20:54:43 +0300

    'adding a post and prevent modifier'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template> <!--секция для разметки-->
   <div class="app">
-      <form>
-        <h4>Создание поста</h4>
-        <!-- v-bind позволяет связывать данные с конкретным компонентом здесь связываю инпут с title
-        подписываюсь на изменения при помощи @input и встроенной функции $event-->
-        <input
-            v-bind:value="title"
-            @input="title = $event.target.value"
-            class="input"
-            type="text"
-            placeholder="название поста">
-        <!--тоже самое для body-->
-        <input
-            v-bind:value="body"
-            @input="body = $event.target.value"
-            class="input"
-            type="text"
-            placeholder="описание поста">
-        <button class="btn" @click="createPost"> Добавить</button>
-      </form>
+    <form @submit.prevent> <!--отменяю стандартное поведения браузера на событие submit при помощи preventDefault-->
+      <h4>Создание поста</h4>
+      <!-- v-bind позволяет связывать данные с конкретным компонентом здесь связываю инпут с title
+      подписываюсь на изменения при помощи @input и встроенной функции $event-->
+      <input
+          v-bind:value="title"
+          @input="title = $event.target.value"
+          class="input"
+          type="text"
+          placeholder="название поста">
+      <!--тоже самое для body-->
+      <input
+          v-bind:value="body"
+          @input="body = $event.target.value"
+          class="input"
+          type="text"
+          placeholder="описание поста">
+      <button class="btn" @click="createPost"> Добавить</button>
+    </form>
 
     <div class="post" v-for="post in posts"> <!--директива v-for для отрисовки элементов массива-->
-      <div><strong>Название:</strong>{{post.title}}</div> <!--динамически забираю название из post-->
-      <div><strong>Описание:</strong>{{post.body}}</div>
+      <div><strong>Название:</strong>{{ post.title }}</div> <!--динамически забираю название из post-->
+      <div><strong>Описание:</strong>{{ post.body }}</div>
     </div>
   </div>
 </template>
@@ -40,8 +40,16 @@ export default {
       body: ''
     }
   },
-  methods: { /*поле для функций*/
+  methods: {
     createPost() {
+      const newPost = { /*описываю объект пост в константу*/
+        id: Date.now(),
+        title: this.title, /*получаю значения из соответствующей модели*/
+        body:  this.body
+      }
+      this.posts.push(newPost); /*добавление нового объекта в массив постов*/
+      this.title = ''; /*очищаю поле  после добавления поста*/
+      this.body = '';
 
     },
 
@@ -71,12 +79,14 @@ form {
   border: 2px solid teal;
   margin-top: 15px;
 }
+
 .input {
   width: 100%;
   border: 2px solid teal;
   padding: 15px;
   margin-top: 15px;
 }
+
 .btn {
   align-self: flex-end;
   margin-top: 15px;
