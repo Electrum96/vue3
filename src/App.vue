4475d4ba637597107c393d28d87ab82049af4422
Electrum <kachalina.kate96@mail.ru>
2024-03-02 19:47:56 +0300

    'v-on event listener'

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template> <!--секция для разметки-->
   <div>
+    <div >
+      <button v-on:click="addLike">Like</button> <!--связывание функций с кнопками при помощи директивы v-on (слушатель события)-->
+      <button v-on:click="addDislike">Dislike</button>
+
+    </div>
+    <div>
    Кол-во лайков: <strong>{{likes}}</strong>  <!--модель встраивается при помощи интерполяции-->
-    Кол-во лайков: <strong>{{dislikes}}</strong>
+    Кол-во дизлайков: <strong>{{dislikes}}</strong>
+    </div>
   </div>
 </template>
 
@@ -12,7 +19,16 @@ export default {
        likes: 0,   /*описание модели*/
        dislikes: 5
      }
-   }
+   },
+  methods: { /*поле для функций*/
+     addLike() {
+       this.likes += 1;  /*получение доступа к модели внутри функции*/
+     },
+    addDislike() {
+      this.dislikes += 1;
+
+    }
+  }
 
 }
 </script>
