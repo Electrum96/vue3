28243c2d8bce634e150bc85bcf2f0b74ec04f1b4
Electrum <kachalina.kate96@mail.ru>
2024-03-04 17:41:49 +0300

    'component lifecycle: request to the server in mounted hook'

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template> <!--секция для разметки-->
   <div class="app">
     <h1>Страница с постами</h1>
-    <my-button @click="fetchPosts">Получить посты</my-button>
+    <my-button @click="showDialog" style="margin: 15px 0px">Создать пост</my-button>
+    <!--запуск функции меняющей булевое значение в модели-->
 
-    <my-button @click="showDialog" style="margin: 15px 0px">Создать пост</my-button> <!--запуск функции меняющей булевое значение в модели-->
-
-    <my-dialog v-model:show="dialogVisible" >
+    <my-dialog v-model:show="dialogVisible">
 
       <post-form @create="createPost"/>
 
     </my-dialog> <!--добавляю в модальное окно форму-->
 
-    <post-list :posts="posts"  @remove="removePost"/>  <!-- вешаю слушатель @remove и передаю туда функцию удаления поста-->
+    <post-list :posts="posts" @remove="removePost"/>
+    <!-- вешаю слушатель @remove и передаю туда функцию удаления поста-->
   </div>
 </template>
 
@@ -21,6 +21,7 @@ import PostList from "@/components/PostList.vue";
 import MyDialog from "@/components/UI/MyDialog.vue";
 import MyButton from "@/components/UI/MyButton.vue";
 import axios from "axios";
+
 export default {
 
   components: {
@@ -31,10 +32,7 @@ export default {
   },
   data() {
     return {
-      posts: [
-
-
-      ],
+      posts: [],
       dialogVisible: false, /*входная модель в зависимости от которой модальное окно лио видно или скрыто*/
       modificatorValue: ''
     }
@@ -49,17 +47,24 @@ export default {
       this.posts = this.posts.filter(p => p.id !== post.id) /*функция принимает пост проходится фильтром и возвращает массив элементов без поста */
     },
     showDialog() {
-     this.dialogVisible = true; /*изменение булевого значения для модели*/
+      this.dialogVisible = true; /*изменение булевого значения для модели*/
     },
     async fetchPosts() {
-         try {
-           const  responce = await  axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
-           this.posts = responce.data;
-         } catch (e) {
-           alert('Ошибка')
-         }
+      try {
+        setTimeout(async () => {
+          const responce = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+          this.posts = responce.data;
+        }, 1000)
+
+
+      } catch (e) {
+        alert('Ошибка')
+      }
     }
 
+  },
+  mounted() {
+    this.fetchPosts();
   }
 
 }
