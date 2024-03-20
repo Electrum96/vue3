c81dbe0951acf625b788218e2aabfa752cefa7f9
Electrum <kachalina.kate96@mail.ru>
2024-03-20 11:40:43 +0300

    '20.03'

---
+++ src/components/UI/MySelect.vue
@@ -0,0 +1,32 @@
+<!--<template>-->
+<!--  <select>-->
+<!--    <option disabled value="">Выберите из списка</option>-->
+<!--    <option-->
+<!--        v-for="option in options"-->
+<!--        :key="option.value"-->
+<!--        :value="option.value"-->
+<!--    >-->
+<!--      {{ option.name}}-->
+
+<!--    </option>-->
+<!--  </select>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--export default {-->
+<!--  name: 'my-select',-->
+<!--  props: {-->
+<!--    modelValue: {-->
+<!--      type: String-->
+<!--    },-->
+<!--    options: {-->
+<!--      type: Array,-->
+<!--      default: () => []-->
+<!--    }-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
+
+<!--<style  scoped>-->
+
+<!--</style>-->
